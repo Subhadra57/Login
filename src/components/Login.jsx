5ab8631e05a3bdfff710063522a7217298825e1c
Subhadra57 <sapkotasubhadra30@gmail.com>
2023-10-06 12:07:57 +0545

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+  <>
+  <form action="">
+    <div>
+      <label htmlFor="username">Username</label>
+      <input type="text" name="username" id="Username"/>
+    </div>
+
+    <div>
+      <label htmlFor="password">Password</label>
+      <input type="text" name="password" id="Password"/>
+    </div>
+
+    <button type="submit">Submit</button>
+
+  </form>
+  </>
   )
 }
 
